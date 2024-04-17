787db30136caf17d20c36bc01ac08148235d675e
Niklas Sandén <sandenniklas@gmail.com>
2024-04-17 20:18:24 +0200

    Use mean grad during integration

--- src/integrate_surface.m
+++ src/integrate_surface.m
@@ -3,7 +3,8 @@ function [depths, grads] = integrate_surface(normal_image, mask)
     sz = size(normal_image(:,:,1));
 
     grads = -normal_image(:,:,1:2) ./ (normal_image(:,:,3) + (normal_image(:,:,3) == 0));
-    grads = min(max(grads, -10), 10);
+    grad_clamp = 10;
+    grads = min(max(grads, -grad_clamp), grad_clamp);
 
     to_ind = @(r, c) sub2ind(sz, r, c);
 
@@ -18,16 +19,19 @@ function [depths, grads] = integrate_surface(normal_image, mask)
     for r = 1:sz(1)
         for c = 1:sz(2)
             for i = 1:length(dirs)
-                if r+dirs{i}(1) >= 1 && r+dirs{i}(1) <= sz(1) && c+dirs{i}(2) >= 1 && c+dirs{i}(2) <= sz(2)
+                r2 = r + dirs{i}(1);
+                c2 = c + dirs{i}(2);
+                if r2 >= 1 && r2 <= sz(1) && c2 >= 1 && c2 <= sz(2)
                     rows(end+1) = eq;
                     cols(end+1) = to_ind(r, c);
                     values(end+1) = 1;
 
                     rows(end+1) = eq;
-                    cols(end+1) = to_ind(r+dirs{i}(1), c+dirs{i}(2));
+                    cols(end+1) = to_ind(r2, c2);
                     values(end+1) = -1;
 
-                    b(end+1) = signs{i} * grads(r+dirs{i}(1), c+dirs{i}(2), dims{i});
+                    b(end+1) = signs{i} * ...
+                        (grads(r2, c2, dims{i}) + grads(r, c, dims{i})) / 2;
 
                     eq = eq + 1;
                 end
